44f43232a3d6d887184703328de889b6104a5e60
“knoweski <Krystle.noweski@gmail.com ~$ git config --global user.name knoweski>
2020-01-27 16:09:55 -0500

    update

--- HashMaps_drills.js
+++ HashMaps_drills.js
@@ -1,22 +1,72 @@
-const HashMap = require('./hashMap')
+//1,3,5,7
 
-HashMap.MAX_LOAD_RATIO = 0.5;
-HashMap.SIZE_RATIO = 3;
+//1
+const HashMap = require('./HashMaps_drills');
 
 function main() {
-  const lotr = new HashMap()
-  const data = [
-    { "Hobbit": "Bilbo" },
-    { "Hobbit": "Frodo" },
-    { "Wizard": "Gandolf" },
-    { "Human": "Aragorn" },
-    { "Elf": "Legolas" },
-    { "Maiar": "The Necromancer" },
-    { "Maiar": "Sauron" },
-    { "RingBearer": "Gollum" },
-    { "LadyOfLight": "Galadriel" },
-    { "HalfElven": "Arwen" },
-    { "Ent": "Treebeard" }
+  const lor = new HashMap();
 
-  ];
+  lor.MAX_LOAD_RATIO = 0.5;
+
+  lor.set('Hobbit', 'Bilbo');
+  lor.set('Hobbit', 'Frodo');
+  lor.set('Wizard', 'Gandolf');
+  lor.set('Human', 'Aragon');
+  lor.set('Elf', 'Legolas');
+  lor.set('Maiar', 'The Necromancer');
+  lor.set('Maiar', 'Sauron');
+  lor.set('RingBearer', 'Gollum');
+  lor.set('LadyOfLight', 'Galadriel');
+  lor.set('HalfElven', 'Arwen');
+  lor.set('Ent', 'Treebeard');
+
+
+    return lor;
+
 }
+
+main();
+
+//3
+
+
+//5
+
+function palindrome(str) {
+    let map = new Map();
+    let unmatched = false;
+    for (let i = 0; i < str.length; i++) {
+      let char = str[i];
+      if (map.has(char)) {
+        let value = map.get(char);
+        map.set(char, value + 1);
+      } else {
+        map.set(char, 1);
+      }
+    }
+    for( let value of map.values()){
+      if( value % 2 === 1  ){
+        if(unmatched){
+          return false;
+        }else{
+          unmatched = true;
+        }
+      }
+    }
+    return true;
+  }
+  console.log(palindrome('north'));
+  
+  function removeDuplicates2(str) {
+    const stringHolder = new Map();
+    let result = '';
+    str.split('').forEach(letter => {
+      if (!stringHolder.has(letter)) {
+        stringHolder.set(letter, '');
+        result += letter;
+      }
+    });
+    return result;
+  }
+
+  //7
