cd7b8b7ddb45885661fd5018843f985f4116b36e
Mason Reichbauer <mreichba@mail.usf.edu>
2020-01-28 10:14:21 -0500

    added hashmap

--- HashMaps_drills.js
+++ HashMaps_drills.js
@@ -1,27 +1,27 @@
 //1,3,5,7
 
 //1
-const HashMap = require('./HashMaps_drills');
+const HashMap = require('./HashMap');
 
 function main() {
-  const lor = new HashMap();
+  const lotr = new HashMap();
 
-  lor.MAX_LOAD_RATIO = 0.5;
+  lotr.MAX_LOAD_RATIO = 0.5;
 
-  lor.set('Hobbit', 'Bilbo');
-  lor.set('Hobbit', 'Frodo');
-  lor.set('Wizard', 'Gandolf');
-  lor.set('Human', 'Aragon');
-  lor.set('Elf', 'Legolas');
-  lor.set('Maiar', 'The Necromancer');
-  lor.set('Maiar', 'Sauron');
-  lor.set('RingBearer', 'Gollum');
-  lor.set('LadyOfLight', 'Galadriel');
-  lor.set('HalfElven', 'Arwen');
-  lor.set('Ent', 'Treebeard');
+  lotr.set('Hobbit', 'Bilbo');
+  lotr.set('Hobbit', 'Frodo');
+  lotr.set('Wizard', 'Gandolf');
+  lotr.set('Human', 'Aragon');
+  lotr.set('Elf', 'Legolas');
+  lotr.set('Maiar', 'The Necromancer');
+  lotr.set('Maiar', 'Sauron');
+  lotr.set('RingBearer', 'Gollum');
+  lotr.set('LadyOfLight', 'Galadriel');
+  lotr.set('HalfElven', 'Arwen');
+  lotr.set('Ent', 'Treebeard');
 
 
-    return lor;
+  return lotr;
 
 }
 
@@ -33,40 +33,40 @@ main();
 //5
 
 function palindrome(str) {
-    let map = new Map();
-    let unmatched = false;
-    for (let i = 0; i < str.length; i++) {
-      let char = str[i];
-      if (map.has(char)) {
-        let value = map.get(char);
-        map.set(char, value + 1);
+  let map = new Map();
+  let unmatched = false;
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (map.has(char)) {
+      let value = map.get(char);
+      map.set(char, value + 1);
+    } else {
+      map.set(char, 1);
+    }
+  }
+  for (let value of map.values()) {
+    if (value % 2 === 1) {
+      if (unmatched) {
+        return false;
       } else {
-        map.set(char, 1);
+        unmatched = true;
       }
     }
-    for( let value of map.values()){
-      if( value % 2 === 1  ){
-        if(unmatched){
-          return false;
-        }else{
-          unmatched = true;
-        }
-      }
-    }
-    return true;
   }
-  console.log(palindrome('north'));
-  
-  function removeDuplicates2(str) {
-    const stringHolder = new Map();
-    let result = '';
-    str.split('').forEach(letter => {
-      if (!stringHolder.has(letter)) {
-        stringHolder.set(letter, '');
-        result += letter;
-      }
-    });
-    return result;
-  }
+  return true;
+}
+console.log(palindrome('north'));
 
-  //7
+function removeDuplicates2(str) {
+  const stringHolder = new Map();
+  let result = '';
+  str.split('').forEach(letter => {
+    if (!stringHolder.has(letter)) {
+      stringHolder.set(letter, '');
+      result += letter;
+    }
+  });
+  return result;
+}
+
+//7
